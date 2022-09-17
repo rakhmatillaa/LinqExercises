f0c5c03a2350bbdeeb1746e9af16ac7fc4633f7f
rakhmatillaa <111976636+rakhmatillaa@users.noreply.github.com>
2022-09-17 20:08:54 +0500

    Update exercise3.cs

--- exercise3.cs
+++ exercise3.cs
@@ -5,19 +5,15 @@ using System.Collections.Generic;
 class Program
 {
     static void Main(string[] args)
-    {
-     List<int> numberList=new List<int>();
-        numberList.Add(1);
-        numberList.Add(-34);
-        numberList.Add(32);
-        numberList.Add(16);
-        numberList.Add(0);
-        numberList.Add(67);
-        numberList.Add(-4);
-
+    { 
+     List<int> numberList=new List<int>() {1,-34,32,16,0,67,-4};
+        
         var sortedNumbers = numberList.OrderBy(x => x);
-
+        
         foreach (int number in sortedNumbers)
+        {
             Console.WriteLine(number);
+        }
+        
     }
 }
